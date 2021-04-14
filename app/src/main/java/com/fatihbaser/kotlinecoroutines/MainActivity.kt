342ac8d996c1181a71e2454b46d029f300ec82e6
fatih40 <baserfatih40@gmail.com>
2021-04-14 18:32:10 +0200

    x

--- app/src/main/java/com/fatihbaser/kotlinecoroutines/MainActivity.kt
+++ app/src/main/java/com/fatihbaser/kotlinecoroutines/MainActivity.kt
@@ -2,10 +2,38 @@ package com.fatihbaser.kotlinecoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //light weightness  treadlere gore daha iyidir
+
+        //GlobalScope bu bir kapsam
+        //Coroutine olusturuYORUZ ama 100000kere olusuruyoruz
+       /* GlobalScope.launch { repeat(100000) {
+            launch {
+                println("android")
+            }
+        } }
+*/
+        // Scope  kapsam demek
+        //Coroutines  nerede calisacagini yasam dongusunu belirleyen
+        //Global Scope   butun aplicationda calisacak kapsamda  olusturuyor
+        //RunBlocking
+        //CoroutineScope
+
+        /*runBlocking {
+            launch {
+            delay(2000)
+            println("run blocing")
+        }}*/
+
+       /* GlobalScope.launch { delay(5000)
+        println("run blocing end")}*/
+
+
     }
 }
